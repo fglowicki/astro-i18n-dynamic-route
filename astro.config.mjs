27631c01ac7efd33df1db87040b4a4da47c26797
fglowicki <f.glowicki@p-programisci.pl>
2024-07-15 13:03:37 +0200

    feat: Add React and Vue into the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import { i18nConfig } from "./src/i18n/config";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 
+// https://astro.build/config
 export default defineConfig({
   i18n: i18nConfig,
   site: "http://localhost:4321/", // Site URL from env
-  integrations: [sitemap()],
+  integrations: [sitemap(), react(), vue()],
 });
