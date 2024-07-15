b5a17b7e62a70797f4e88dad7ea18b6af33133a7
fglowicki <f.glowicki@p-programisci.pl>
2024-07-15 08:53:25 +0200

    feat: Add sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import { i18nConfig } from "./src/i18n/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   i18n: i18nConfig,
+  site: "http://localhost:4321/", // Site URL from env
+  integrations: [sitemap()],
 });
