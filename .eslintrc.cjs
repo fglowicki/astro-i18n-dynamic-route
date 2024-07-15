c12722219b6bd6e4e35a500a51e7668e74ad711b
fglowicki <f.glowicki@p-programisci.pl>
2024-07-15 11:31:03 +0200

    feat: Add Eslint configuration for astro

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["@sparing-software/eslint-config", "plugin:astro/recommended"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // All rules: https://ota-meshi.github.io/eslint-plugin-astro/rules/
+      },
+    },
+  ],
+};
