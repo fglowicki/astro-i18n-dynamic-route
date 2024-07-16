fccfee02479fbbcab7213e0892b75510364d4f30
fglowicki <f.glowicki@p-programisci.pl>
2024-07-16 08:42:28 +0200

    feat: Add SSR implementation into the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { i18nConfig } from './src/i18n/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import vue from '@astrojs/vue'
-
 import partytown from '@astrojs/partytown'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   i18n: i18nConfig,
   site: 'http://localhost:4321/', // Site URL from env
-  integrations: [sitemap(), react(), vue(), partytown()]
+  integrations: [sitemap(), react(), vue(), partytown()],
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  })
 })
